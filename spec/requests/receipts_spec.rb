ec182a0ad44d985be625e91918dda9503334d35e
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-09 20:53:30 +0200

    Created the necessary factories for specs purposes

--- spec/requests/receipts_spec.rb
+++ spec/requests/receipts_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Receipts", type: :request do
-  let!(:user) {User.create!(email: "testtest@example.com", last_name:"Saint Frison", first_name: "Agathe", password: "Test.123", password_confirmation: "Test.123")}
-  let(:retailer) {Retailer.create!(name: "Test Market", 
-    email: "testmarket@example.com",
-    full_address: "36 rue du test", 
-    zip_code: "75018",
-    city: "Paris",
-    password: "Test.123", 
-    password_confirmation: "Test.123"
-    )}
-  let(:till) {Till.create!(retailer: retailer, reference: 1)}
-  let(:receipt) {Receipt.create!(till: till, reference: "TEST0001", user: user)}
-  let(:carrot) {Product.create!(name: "carrot", kind: "Food and beverages")}
-  let(:item_carrot) {Item.create!(product: carrot, retailer: retailer)}
-  let!(:receipt_line) {ReceiptLine.create!(receipt: receipt, quantity: 2, unit_price_cents: 99, taxe_rate: 20, item: item_carrot)}
+  let!(:user) {create(:user)}
+  let(:retailer) {create(:retailer)}
+  let(:till) {create(:till)}
+  let(:receipt) {create(:receipt)}
+  let(:carrot) {create(:product)}
+  let(:item_carrot) {create(:item, product: carrot, retailer: retailer)}
+  let!(:receipt_line) {create(:receipt_line, item: item_carrot, receipt: receipt)}
+  
   before {sign_in user}
+
+
   describe "GET /index" do
     it "returns http success" do
       get "/receipts/index" 
       expect(response).to have_http_status(:success)
       expect(assigns(:receipts)).to eq(user.receipts)
+    end
+
+    fdescribe "filters" do
+      it "returns http success" do
+        
+      end
     end
   end
 
