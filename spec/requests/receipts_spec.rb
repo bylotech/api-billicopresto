d8df92d7b59fe705f82459f9d6d773d65243d6d8
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-12 16:10:55 +0200

    Wrote the specs for the filter

--- spec/requests/receipts_spec.rb
+++ spec/requests/receipts_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe "Receipts", type: :request do
   let!(:user) {create(:user)}
   let(:retailer) {create(:retailer)}
   let(:till) {create(:till)}
-  let(:receipt) {create(:receipt, date: "2020-12-31")}
   let(:carrot) {create(:product)}
   let(:item_carrot) {create(:item, product: carrot, retailer: retailer)}
-  let!(:receipt_line) {create(:receipt_line, item: item_carrot, receipt: receipt)}
+  let!(:receipt_line) {create(:receipt_line, item: item_carrot, receipt: receipt, quantity: 2, unit_price_cents: 1000)}
+  let!(:receipt) {create(:receipt, date: "2020-12-31")}
+  
 
   before {sign_in user}
 
@@ -75,7 +76,7 @@ RSpec.describe "Receipts", type: :request do
               end
             end
             context "not" do
-              context "when no receipts have the same date than filyter" do
+              context "when no receipts have the same date than filter" do
                 it "returns nothing" do
                   get "/receipts/index", params: { filters: {date_operator:"not", date: "2021-01-01"}}
                   expect(response).to have_http_status(:success)
@@ -92,6 +93,159 @@ RSpec.describe "Receipts", type: :request do
             end
           end
       end
+      context "amount_including_taxes" do
+        let!(:another_receipt_line) {create(:receipt_line, item: item_carrot, receipt: another_receipt, quantity: 3, unit_price_cents: 1000)}
+        let!(:another_receipt) {create(:receipt)}
+        context "with no operator" do
+          context "when no receipt matches the filter amount" do
+            it "returns nothing" do
+              get "/receipts/index", params: { filters: {amount_including_taxes: 2000}}
+              expect(response).to have_http_status(:success)
+              expect(assigns(:receipts)).to eq([])
+            end
+          end
+  
+          context "when no receipt matches the filter amount" do
+            it "returns the receipt" do
+              get "/receipts/index", params: { filters: {amount_including_taxes: another_receipt.amount_including_taxes}}
+              expect(response).to have_http_status(:success)
+              expect(assigns(:receipts)).to eq([another_receipt])
+            end
+          end
+        end
+        context "with operator" do
+          context "less" do
+            context "when no receipts have a amount less than the filter value" do
+              it "returns nothing" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"less", amount_including_taxes: 20}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq([])
+              end
+            end
+            context "when there are receipts with a amount less than the filter value" do
+              it "returns nothing" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"less", amount_including_taxes: 25}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq([receipt])
+              end
+            end
+          end
+          context "more" do
+            context "when no receipts have an amount above the filter value" do
+              it "returns nothing" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"more", amount_including_taxes: 40}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq([])
+              end
+            end
+            context "when there are receipts with a amount less than the filter value" do
+              it "returns nothing" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"more", amount_including_taxes: 30}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq([another_receipt])
+              end
+            end
+          end
+        context "not" do
+            context "when no receipts have the same amount than filter" do
+              it "returns nothing" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"not", amount_including_taxes: 30}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq(user.receipts)
+              end
+            end
+            context "when there are receipts with the same amount than the filter value" do
+              it "returns the receipts that is not matching the filter value" do
+                get "/receipts/index", params: { filters: {amount_including_taxes_operator:"not", amount_including_taxes: receipt.amount_including_taxes}}
+                expect(response).to have_http_status(:success)
+                expect(assigns(:receipts)).to eq([another_receipt])
+              end
+            end
+          end
+        end
+      end
+
+      context "till.retailer" do
+        let(:another_retailer) {create(:retailer)}
+        let!(:another_receipt_line) {create(:receipt_line, item: item_carrot, receipt: another_receipt, quantity: 3, unit_price_cents: 1000)}
+        let!(:another_receipt) {create(:receipt, till: another_till)}
+        let!(:another_till) {create(:till, retailer: another_retailer)}
+        context "with receipts that matches the filter" do
+          it "returns the correct receipts" do
+            get "/receipts/index", params: { filters: {"till.retailer" => another_retailer.id}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([another_receipt])
+          end
+        end
+
+        context "with no receipts that matches the filter" do
+          it "returns nothing" do
+            get "/receipts/index", params: { filters: {"retailer.city" => another_retailer.id + 1 }}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([])
+          end
+        end
+
+        context "with the not operator" do
+          it "returns the correct receipts" do
+            get "/receipts/index", params: { filters: {"till.retailer_operator" => "not", "till.retailer" => another_retailer.id}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([receipt])
+          end
+        end
+      end
+
+      context "retailer.city" do
+        let(:another_retailer) {create(:retailer, city: "Reims")}
+        let!(:another_receipt_line) {create(:receipt_line, item: item_carrot, receipt: another_receipt, quantity: 3, unit_price_cents: 1000)}
+        let!(:another_receipt) {create(:receipt, till: another_till)}
+        let!(:another_till) {create(:till, retailer: another_retailer)}
+        context "with receipts that matches the filter" do
+          it "returns the correct receipts" do
+            get "/receipts/index", params: { filters: {"retailer.city" => "Reims"}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([another_receipt])
+          end
+        end
+
+        context "with no receipts that matches the filter" do
+          it "returns nothing" do
+            get "/receipts/index", params: { filters: {"retailer.city" => "Amiens"}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([])
+          end
+        end
+
+        context "with the not operator" do
+          it "returns the correct receipts" do
+            get "/receipts/index", params: { filters: {"retailer.city_operator" => "not", "retailer.city" => "Reims"}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([receipt])
+          end
+        end
+      end
+
+      context "status" do
+        let(:another_retailer) {create(:retailer)}
+        let!(:another_receipt_line) {create(:receipt_line, item: item_carrot, receipt: another_receipt, quantity: 3, unit_price_cents: 1000)}
+        let!(:another_receipt) {create(:receipt, till: another_till, status: :archived)}
+        let!(:another_till) {create(:till, retailer: another_retailer)}
+        context "active" do
+          it "returns only the active receipts" do
+            get "/receipts/index", params: { filters: {status: "active"}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([receipt])
+          end
+        end
+        context "archived" do
+          it "returns only the active receipts" do
+            get "/receipts/index", params: { filters: {status: "archived"}}
+            expect(response).to have_http_status(:success)
+            expect(assigns(:receipts)).to eq([another_receipt])
+          end
+        end
+      end
+
     end
   end
 
