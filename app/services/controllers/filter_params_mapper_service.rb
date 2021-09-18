7413ff0ee654b6801ded39cd5b17341bec974b2a
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-18 19:00:38 +0200

    Remove the money filed constant and update the service to use the filter_whitelist object

--- app/services/controllers/filter_params_mapper_service.rb
+++ app/services/controllers/filter_params_mapper_service.rb
@@ -1,46 +1,30 @@
 class Controllers::FilterParamsMapperService
 
-  MONEY_FIELDS = %w[
-    unit_price
-    amount_excluding_taxes
-    amount_including_taxes
-    amount_taxes
-  ]
-
   SUFFIX_OPERATOR = %w[
     _less
     _more
     _not]
     
-  def initialize(filters, filter_params)
+  def initialize(filters, filter_params, filter_params_whitelist)
     @filters = filters
     @filter_params = filter_params
+    @filter_params_whitelist = filter_params_whitelist
   end
 
   def call!
+    map_keys
+    convert_cents
     add_operators
-    map_filters
     @filters
   end
 
   private
 
-  def map_filters
-    convert_cents
-  end
-
   def convert_cents
-
-    regex = "^(#{MONEY_FIELDS.join('|')})+(#{SUFFIX_OPERATOR.join('|')}|)$"
-
-    money_fields = @filters.select{|key, value| key.match(/#{regex}/) }
-
+    money_fields = @filters.select{|key, value| key.end_with?("_cents")}
     money_fields.each do |key,value|
-      @filters[key_cents(key)] = value.to_f*100.to_i
-      @filters.delete(key)
+      @filters[key] = value.to_f*100.to_i
     end
-
-    @filters
   end
 
   def key_cents(key)
@@ -51,11 +35,22 @@ class Controllers::FilterParamsMapperService
 
   def add_operators
     filter_operators = @filter_params.select{|k,v| k.end_with?("_operator") }
+    
     filter_operators.each do |filter_operator, value|
       field = filter_operator.delete_suffix("_operator")
-      next unless @filters[field]
-      @filters["#{field}_#{value}"] = @filters[field]
-      @filters.delete(field)
+      mapped_field = @filter_params_whitelist.dig(field, :mapped_field) || field
+      filter_value = @filters[mapped_field]
+      next unless filter_value
+      @filters["#{mapped_field}_#{value}"] = filter_value
+      @filters.delete(mapped_field)
+    end
+  end
+
+  def map_keys
+    keys_to_map = @filter_params_whitelist.select{|k,v| v[:mapped_field] && @filters[k]}
+    keys_to_map.each do |k,v|
+      @filters[v[:mapped_field]] = @filters[k]
+      @filters.delete(k)
     end
   end
 
