8eb87a40176324bced296d6658b38041276d664a
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-29 22:21:35 +0200

    create the filter action, add the field we want to filter in FIELD_FILTER_WHITELIST and ... BOOOOOOOOM it works directly perfectly 🔥🔥🔥🔥🔥🔥🔥🔥

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -3,13 +3,16 @@ class VouchersController < ApplicationController
   load_and_authorize_resource
 
   FIELD_FILTER_WHITELIST = [
-    
+    'items.product_id'
   ]
 
   def index
+    @vouchers = @vouchers.joins(:item) if filter_params&['items.product_id']
     @vouchers =  Controllers::FilterService.new(@vouchers,FIELD_FILTER_WHITELIST, filter_params).filter! if filter_params
+  end
 
-
+  def filter
+    @products = Product.joins(:items).where(items: {retailer: current_retailer})
   end
 
   def show
