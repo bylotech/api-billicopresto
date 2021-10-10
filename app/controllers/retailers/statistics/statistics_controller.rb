f5f590fd27f2a157fc0aab912e9638e0f30f04d0
Tom Ecrepont <tomecrepont@gmail.com>
2021-10-10 21:23:08 +0200

    Created the other end points /sales_by_users_value and /sales_by_products_value that will return the graphes

--- app/controllers/retailers/statistics/statistics_controller.rb
+++ app/controllers/retailers/statistics/statistics_controller.rb
@@ -7,6 +7,16 @@ module Retailers
       def users_by_zipcode
         UsersByZipcodeService.new(current_retailer).call!
       end
+
+      # /sales_by_users_value
+      def sales_by_users_value
+        SalesByUsersValueService.new(current_retailer).call!
+      end
+
+      # /sales_by_products_value
+      def sales_by_products_value
+        SalesByProductValueService.new(current_retailer).call!
+      end
     end
   end
 end
