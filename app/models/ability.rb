7caf304d2e0d0a958bca0951721b27168bfad305
Tom Ecrepont <tomecrepont@gmail.com>
2021-10-11 22:47:57 +0200

    remove unneccessary ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,6 @@ class Ability
       can [:read, :filter], Receipt, user: user
     when "Retailer"
       can [:read, :filter, :create], Vouchers::Voucher, retailer: user
-      can [:statistics], Retailer
     end
   end
 end
