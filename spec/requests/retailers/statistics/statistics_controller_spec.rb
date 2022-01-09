6a20875ef30f001e8d885aeeca17d53e52ec1abf
Tom Ecrepont <tomecrepont@gmail.com>
2022-01-09 14:22:05 +0100

    set up specs for users_by_zipcode

--- spec/requests/retailers/statistics/statistics_controller_spec.rb
+++ spec/requests/retailers/statistics/statistics_controller_spec.rb
@@ -6,5 +6,20 @@ require "./spec/support/shared_examples/retailer_authenticated_page_example"
 RSpec.describe "Retailers::Statistics::StatisticsController", type: :request do
   describe "GET /users_by_zipcode" do
     it_behaves_like "retailer authenticated page", "/retailers/statistics/users_by_zipcode"
+    context "calls the correct service" do
+      let(:retailer) { create(:retailer) }
+      before { sign_in retailer }
+      it "and returns the correct template" do
+        service_double = instance_double(Retailers::Statistics::UsersByZipcodeService)
+        allow(Retailers::Statistics::UsersByZipcodeService).to(
+          receive(:new)
+          .with(retailer)
+          .and_return(service_double),
+        )
+        expect(service_double).to receive(:call!)
+        get "/retailers/statistics/users_by_zipcode"
+        expect(response).to render_template("retailers/statistics/users_by_zipcode")
+      end
+    end
   end
 end
